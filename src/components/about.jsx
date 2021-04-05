1c53e691e3552ed520d3bcfee19887fcea938acf
Marlo <marlo.baguyos@outlook.com>
2021-04-05 10:11:47 -0400

    update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./css/about.css";
 
-
 class About extends Component {
-  state = {  }
-  render() { 
+  state = {};
+  render() {
     return (
       <div>
         <h1 className="aboutH1">This is our About Page</h1>
