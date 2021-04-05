e2be1cdb8b9ba5b52d98274ce6527fddf8c8bc5b
Marlo <marlo.baguyos@outlook.com>
2021-04-05 12:43:34 -0400

    update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,12 +5,15 @@ class About extends Component {
   render() {
     return (
       <div>
-        <div class="jumbotron">
-          <h1 class="display-4">Hello, world!</h1>
-          <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-          <hr class="my-4"></hr>
-          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-          <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        <div className="jumbotron">
+          <h1 className="display-4">Marlo Baguyos</h1>
+          <p className="lead">This is an FSDI project on React</p>
+          <hr className="my-4"></hr>
+          <p className="lead">
+            <label>Email: marlo.baguyos@sdgku.edu</label>
+            <label>Phone: marlo.baguyos@sdgku.edu</label>
+            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+          </p>
         </div>
       </div>
     );
