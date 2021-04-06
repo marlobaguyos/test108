3f36282166ad1c1f6594dd92dd80a70e2cca9797
Marlo <marlo.baguyos@outlook.com>
2021-04-06 10:10:03 -0400

    update

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import {connect} from "react-redux";
 
 class NavBar extends Component {
     state = {  }
     render() { 
         return (
             <div>
-                <nav className="navbar navbar-dark bg-dark fixed-top  navbar-expand-lg">
+                <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+                <label class="navbar-brand" href="/">Lapu Lapu</label>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/catalog">Catalog</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/about">About</a>
+                            </li>
+                        </ul>
+                        <span class="form-inline my-2 my-lg-0">
+                            <button button type="button" class="btn btn-secondary my-2 my-sm-0" type="submit">View Cart</button>
+                        </span>
+                    </div>
+                </nav>
+
+
+
+
+
+                {/* <nav className="navbar navbar-dark bg-dark fixed-top  navbar-expand-lg">
                     <div className="container-fluid">
                         <label className="navbar-brand" href="/">Lapu Lapu</label>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,12 +48,23 @@ class NavBar extends Component {
                                 <a className="nav-link" href="/about">About</a>
                                 </li>
                             </ul>
+                            <form class="form-inline my-2 my-lg-0">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            </form>
+                            View Cart: {this.props.cart.length}
                         </div>
                     </div>
-                </nav>
+                </nav> */}
             </div>
         );
     }
 }
 
-export default NavBar;
+const mapStateToProps = (state) => {
+    return{
+        cart: state
+    };
+};
+//read: pass a fn that maps the state to props
+export default connect(mapStateToProps,null)(NavBar);
--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -5,6 +5,11 @@ const cartReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_PRODUCT':
       var copy = [...state];
+
+      /**
+       *  if this product id (action.payload.product.id) does not exist inside the cart then add id
+       * but if it exist (there is a product on the cart with the same product id), then, just increase the quantity of the product in the cart
+       */
       copy.push(action.payload);
       return copy;
     case 'REMOVE_PRODUCT':
