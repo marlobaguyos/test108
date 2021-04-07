9ef0d1ec377070c2ad9040daade5e176329f45eb
Marlo <marlo.baguyos@outlook.com>
2021-04-07 08:45:07 -0400

    update

---
+++ src/components/cart.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Cart extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Your Cart</h1>
+      </div>
+    );
+  }
+}
+
+export default Cart;
