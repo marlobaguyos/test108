583cb1f0bc3f0c04c6ddecd025b60decb1586d0e
Marlo <marlo.baguyos@outlook.com>
2021-04-07 08:46:48 -0400

    update

--- src/App.js
+++ src/App.js
@@ -6,25 +6,52 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import Catalog from './components/catalog';
 import Home from './components/home';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
 import About from './components/about';
+import Cart from './components/cart'
+
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className=" container-fluid App">
-        <NavBar></NavBar>
-  
-        <Switch>
-        <Route path="/" exact component={Home}></Route>
-            <Route path="/home" exact component={Home}></Route>
-            <Route path="/catalog" exact component={Catalog}></Route>
-            <Route path="/about" exact component={About}></Route>
-        </Switch>
-        
-        <Footer></Footer>
-      </div>
-    </BrowserRouter>
+  return ( <
+    BrowserRouter >
+    <
+    div className = " container-fluid App" >
+    <
+    NavBar > < /NavBar>
+
+    <
+    Switch >
+    <
+    Route path = "/"
+    exact component = {
+      Home
+    } > < /Route> <
+    Route path = "/home"
+    exact component = {
+      Home
+    } > < /Route> <
+    Route path = "/catalog"
+    exact component = {
+      Catalog
+    } > < /Route> <
+    Route path = "/about"
+    exact component = {
+      About
+    } > < /Route> <
+    Route path = "/cart"
+    exact component = {
+      Cart
+    } > < /Route> <
+    /Switch>
+
+    <
+    Footer > < /Footer> <
+    /div> <
+    /BrowserRouter>
   );
 }
 
