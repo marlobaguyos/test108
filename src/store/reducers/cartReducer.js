fefd3d88f57b0afd282e8e41a90bf3619080ea6e
Marlo <marlo.baguyos@outlook.com>
2021-04-07 08:40:20 -0400

    update

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -5,25 +5,22 @@ const cartReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_PRODUCT':
       var copy = [...state];
-      if (action.payload.product.id != this.copycopy.product.id) {
-        copy.push(action.payload);
-      } else {
-        copy.push(action.payload);
-        return copy;
+      let found = false;
+      for (let i = 0; i < copy.length; i++) {
+        let item = copy[i];
+        if (item.product.id === action.payload.product.id) {
+          found = true;
+          item.quantity = item.quantity + action.payload.quantity;
+        }
       }
-
-      /**
-       *  if this product id (action.payload.product.id) does not exist inside the cart then add id
-       * but if it exist (there is a product on the cart with the same product id), then, just increase the quantity of the product in the cart
-       */
-
-
-      // copy.push(action.payload);
-      // return copy;
-      case 'REMOVE_PRODUCT':
-        //here
-      default:
-        return state;
+      if (!found) {
+        copy.push(action.payload);
+      }
+      return copy;
+    case 'REMOVE_PRODUCT':
+      //here
+    default:
+      return state;
   }
 };
 
